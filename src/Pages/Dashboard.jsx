b981146fd4b40f1637380f4d093fdc7768fdeeb4
Alisherjon Egamberdiyev <egamberdiyevalisherjon@gmail.com>
2023-03-01 16:26:45 +0500

    dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const { info } = useSelector(({ market }) => market);
 
-export default Dashboard
+  return info ? (
+    <div className="container">
+      <h1>{info.name}</h1>
+      <h3>{info.phone}</h3>
+      <p>{info.inCharge}</p>
+      <p>{info.status}</p>
+      <p>{info.address.city.name.uz}</p>
+      <p>{info.address.region.name.uz}</p>
+      <p>{info.address.full}</p>
+    </div>
+  ) : (
+    <h1>Loading...</h1>
+  );
+};
+
+export default Dashboard;
